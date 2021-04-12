5bbfc528599e1a46941e15d691699cefd5513a09
racross1 <racross1@gmail.com>
2021-04-11 20:16:40 -0700

    first commit

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -6,6 +6,8 @@ class TodosContainer extends Component {
 
   renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
 
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+
   render() {
     return(
       <div>
@@ -21,4 +23,10 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
